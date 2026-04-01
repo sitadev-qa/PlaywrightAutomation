ad717a9fc0ba90657b98a8cf2105098939339368
sitadev-qa <vikas@stockholmitacademy.org>
2026-04-01 17:31:51 +0530

    Added

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,8 +1,20 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
+import * as allure from "allure-js-commons";
 
 test('has title', async ({ page }) => {
+
+    await allure.description("The test checks if an active user with a valid password can sign in to the app.");
+    await allure.epic("Signing in");
+    await allure.feature("Sign in with a password");
+    await allure.story("As an active user, I want to successfully sign in using a valid password");
+    await allure.tags("signin", "ui", "positive");
+    await allure.issue("https://github.com/allure-framework/allure-js/issues/331", "ISSUE-331");
+    await allure.owner("eroshenkoam");
+    await allure.parameter("browser", "chrome");
+
   await page.goto('https://playwright.dev/');
+
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
